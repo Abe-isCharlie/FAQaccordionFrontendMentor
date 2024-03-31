421ff5227f1c4aec51d5990e2f02361fe1cafe50
AB <70777952+Abe-isCharlie@users.noreply.github.com>
2024-03-31 17:58:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,16 +7,13 @@ questions.forEach(function (question) {
 		const plusIcon = this.querySelector('.plus-icon');
 		const minusIcon = this.querySelector('.minus-icon');
 
-		// Verifica se a resposta está sendo clicada pela segunda vez
 		const isSecondClick = answer.classList.contains('show-answer');
 
-		// Oculta todas as respostas antes de mostrar a resposta da pergunta clicada
 		answers.forEach(function (answer) {
 			answer.classList.remove('show-answer');
 			answer.classList.add('hide-answer');
 		});
 
-		// Oculta todos os ícones de menos e mostra todos os ícones de mais antes de alterar o estado
 		document.querySelectorAll('.minus-icon').forEach(function (icon) {
 			icon.classList.add('hide-icon');
 		});
@@ -24,9 +21,7 @@ questions.forEach(function (question) {
 			icon.classList.remove('hide-icon');
 		});
 
-		// Alternar visibilidade da resposta
 		if (isSecondClick) {
-			// Se for o segundo clique, fecha a resposta
 			answer.classList.remove('show-answer');
 			answer.classList.add('hide-answer');
 			plusIcon.classList.remove('hide-icon');
@@ -34,7 +29,6 @@ questions.forEach(function (question) {
 			minusIcon.classList.remove('spin-icon');
 			plusIcon.classList.add('spin-icon');
 		} else {
-			// Se for o primeiro clique, abre a resposta
 			answer.classList.remove('hide-answer');
 			answer.classList.add('show-answer');
 			plusIcon.classList.add('hide-icon');
